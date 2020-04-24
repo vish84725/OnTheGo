654a699f5e684c710d086118c94eb8a755c977a4
Visal <vish847255@gmail.com>
2020-04-24 13:10:05 +0530

    initial commit

--- OnTheGo/Controller/LoginViewController.swift
+++ OnTheGo/Controller/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheGo
 //
 //  Created by Visal Hettiarachchi on 4/24/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
