f7b37d5b450670c8f26c46edb63f4879581673d9
Visal <vish847255@gmail.com>
2020-04-24 13:02:01 +0530

    Initial Commit

---
+++ OnTheGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheGo
+//
+//  Created by Visal Hettiarachchi on 4/24/20.
+//  Copyright © 2020 Visal Hettiarachchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
